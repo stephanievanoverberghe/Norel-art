9575838d1fa8932613e9c0a2789f55d0007d3f4b
Stéphanie Vanoverberghe <orangestreet@live.fr>
2024-03-04 18:43:41 +0100

    header mobile

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+const navItems = document.querySelector('.nav-items');
+const navItem = document.querySelectorAll('.nav-item');
+
+// Menu burger
+// Open and disabled Menu
+
+iconBurger.addEventListener('click', () => {
+    if (navItems.style.display === "block") {
+        navItems.style.display = "none";
+    } else {
+        navItems.style.display = "block";
+    }
+});
+
+// Disabled Menu with nav-item
+
+navItems.addEventListener('click', () => {
+    console.log(navItem);
+    if (navItems.style.display === "block") {
+        navItems.style.display = "none";
+    }
+});
